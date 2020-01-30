21b0eb342995e188fc3943cc1a5f46459dd5ede1
zuroxx <zuroxx@gmail.com>
2020-01-30 07:53:41 -0500

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -61,10 +61,10 @@ CLIENT_WG_IPV6="fd42:42:42::2"
 # read -rp "Client's WireGuard IPv6 " -e -i "$CLIENT_WG_IPV6" CLIENT_WG_IPV6
 
 # Adguard DNS by default
-CLIENT_DNS_1="176.103.130.130"
+CLIENT_DNS_1="1.1.1.1"
 # read -rp "First DNS resolver to use for the client: " -e -i "$CLIENT_DNS_1" CLIENT_DNS_1
 
-CLIENT_DNS_2="176.103.130.131"
+CLIENT_DNS_2="8.8.8.8"
 # read -rp "Second DNS resolver to use for the client: " -e -i "$CLIENT_DNS_2" CLIENT_DNS_2
 
 # Ask for pre-shared symmetric key
